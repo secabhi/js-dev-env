6c418d2950d25ff32a880b2a0c457fef3f9efc9e
Abhishek Sinha <Abhishek Sinha>
2017-01-01 23:06:33 +0530

    file commited

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3001;
+var app =express();
+
+app.get('/',function(req,res){
+  res.sendfile(path.join(__dirname,'../src/index.html'))
+})
+
+app.listen(port,function(err){
+  if(err){
+    console.log('==>',err);
+  }
+  else{
+    open('http://localhost:'+port)
+  }
+})
